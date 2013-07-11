67ff04ef55f52fa22ab0658db07b862ae2a2ffd3
aymsic <asicard@engineyard.com>
2013-07-11 13:05:49 -0700

    return to normal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,3 @@ Listr::Application.configure do
   # config.force_ssl = true
 end
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.smtp_settings = {
-    :authentication => :plain,
-    :address => "smtp.mailgun.org",
-    :port => 587,
-    :domain => EY::Config.get(:mailgun, 'MAILGUN_DOMAIN'),
-    :user_name => EY::Config.get(:mailgun, 'MAILGUN_SMTP_USER'),
-    :password => EY::Config.get(:mailgun, 'MAILGUN_SMTP_PASSWORD')
-}
